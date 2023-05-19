f939b8d95d9ae46408097fb94039f777bc151b63
CEGS <ce_gs@hotmail.com>
2023-05-19 18:49:12 -0400

    el logo de homepage como enlace a biography y la pág se conecta al servidor desde homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "../css/home.css";
 import Navbar from "../components/Navbar";
 import NavbarCell from "../components/NavbarCell";
 import  logo  from "../img/mainLogo.svg";
 import useWindowDimentions from "../hook/useWindowDimentions";
+import Axios from "axios";
 
 function Home() {
     let width = useWindowDimentions();
+
+    useEffect(()=>{
+        Axios.get("https://beethovenserver.onrender.com")
+    },[]);
+
     return(
         <div className="home">
             {width>=900? <Navbar visible={true}/> : <NavbarCell />}
-            <img src={logo} alt="Logo" />
+            <a href="/Biography">
+                <img src={logo} alt="Logo" />
+            </a>  
         </div>
     );
 }
